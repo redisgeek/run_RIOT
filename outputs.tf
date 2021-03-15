bc4d68a157c0ff82fc8c7daafbb881c7972bd1d2
Toby H Ferguson <toby.h.ferguson@gmail.com>
2021-03-15 09:55:55 -0700

    Corrected directory location of redis_riot

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "run_riot" {
-    value = "ssh -i ~/.ssh/${var.ssh_key_name}.pem ec2-user@${aws_instance.riot.public_ip} /ussr/local/bin/riot-redis"
+    value = "ssh -i ~/.ssh/${var.ssh_key_name}.pem ec2-user@${aws_instance.riot.public_ip} /usr/local/bin/riot-redis"
     description = "ssh string to execute riot on target ec2 instance"
 }
