84f52b208823cdc6d00999404c8ada1e05802e42
DaShaun <dashaun@dashaun.com>
2021-11-12 11:42:29 -0600

    remove required version and format

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 locals {
-    host = aws_instance.riot.public_ip
-    user = "ec2-user"
-    program = "/usr/local/bin/riot-redis"
+  host    = aws_instance.riot.public_ip
+  user    = "ec2-user"
+  program = "/usr/local/bin/riot-redis"
 }
 
 output "host" {
-    value = local.host
-    description = "Public IP for riot instance"
+  value       = local.host
+  description = "Public IP for riot instance"
 }
 
 output "user" {
-    value = local.user
-    description = "user with access to host"
+  value       = local.user
+  description = "user with access to host"
 }
 
 output "program" {
-    value = local.program
-    description = "Absolute path to program on host"
+  value       = local.program
+  description = "Absolute path to program on host"
 }
 
 output "run_riot" {
-    value = "ssh -i ~/.ssh/${var.ssh_key_name}.pem ${local.user}@${local.host} ${local.program}"
-    description = "ssh string to execute riot-redis on target ec2 instance"
+  value       = "ssh -i ~/.ssh/${var.ssh_key_name}.pem ${local.user}@${local.host} ${local.program}"
+  description = "ssh string to execute riot-redis on target ec2 instance"
 }
 
 output "run_riot_old" {
-    value = "ssh -i ~/.ssh/${var.ssh_key_name}.pem ec2-user@${aws_instance.riot.public_ip} /usr/local/bin/riot-redis"
-    description = "ssh string to execute riot on target ec2 instance"
+  value       = "ssh -i ~/.ssh/${var.ssh_key_name}.pem ec2-user@${aws_instance.riot.public_ip} /usr/local/bin/riot-redis"
+  description = "ssh string to execute riot on target ec2 instance"
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "region" {
-    description = "Region into which to deploy the EC2 instance"
+  description = "Region into which to deploy the EC2 instance"
 }
 
 variable "linux_ami" {
-    description = "Linux ami to use"
+  description = "Linux ami to use"
 }
 
 variable "instance_type" {
-    description = "instance type to use. Default: t3.micro"
+  description = "instance type to use. Default: t3.micro"
 }
 
 variable "ssh_key_name" {
-    description = "name of ssh key to be added to instance"
+  description = "name of ssh key to be added to instance"
 }
 
 variable "security_group_id" {
-    description = "Id of the security group applied to the instance"
+  description = "Id of the security group applied to the instance"
 }
 
 
